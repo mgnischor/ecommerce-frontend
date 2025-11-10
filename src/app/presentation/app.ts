789d23a3d3d2740a665dd998fd3da3e211ca3135
Miguel Nischor <miguel@nischor.com.br>
2025-11-09 20:56:55 -0400

    code replace RouterOutlet with Layout for improved component structure and enable OnPush change detection

--- src/app/presentation/app.ts
+++ src/app/presentation/app.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Layout } from './components/layout/layout';
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet],
+    imports: [Layout],
     templateUrl: './app.html',
     styleUrl: './app.scss',
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class App {}
