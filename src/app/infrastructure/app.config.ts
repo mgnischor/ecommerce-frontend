a1aed5948010867ae711274bbe385841632111e0
Miguel Nischor <miguel@nischor.com.br>
2025-12-16 12:35:51 -0400

    code: added error and authentication interceptors

--- src/app/infrastructure/app.config.ts
+++ src/app/infrastructure/app.config.ts
@@ -1,16 +1,15 @@
-import {
-    ApplicationConfig,
-    provideBrowserGlobalErrorListeners,
-    provideZonelessChangeDetection,
-} from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 import { routes } from '../application/app.routes';
+import { authInterceptor, errorInterceptor } from './interceptors';
 
 export const appConfig: ApplicationConfig = {
     providers: [
         provideBrowserGlobalErrorListeners(),
         provideZonelessChangeDetection(),
         provideRouter(routes),
+        provideHttpClient(withInterceptors([authInterceptor, errorInterceptor])),
     ],
 };
