e3c68f2493c96cb375673c77cff3e6211a71bab3
Miguel Nischor <miguel@nischor.com.br>
2026-04-10 09:15:54 -0400

    refactor: fix formatting in error interceptor for consistency

--- src/app/infrastructure/interceptors/error.interceptor.ts
+++ src/app/infrastructure/interceptors/error.interceptor.ts
@@ -18,6 +18,6 @@ export const errorInterceptor: HttpInterceptorFn = (req, next) => {
 
             // Retornar o erro para ser tratado pelos componentes
             return throwError(() => error);
-        })
+        }),
     );
 };
